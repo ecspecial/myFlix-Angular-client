294753a96ebd38258c141126a751c84bc86d63dc
ecspecial <106328797+ecspecial@users.noreply.github.com>
2023-05-13 04:41:13 +0300

    Added movie director dialog

---
+++ src/app/director-view/director-view.component.ts
@@ -0,0 +1,18 @@
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-director-view',
+  templateUrl: './director-view.component.html',
+  styleUrls: ['./director-view.component.scss']
+})
+export class DirectorViewComponent {
+  constructor(
+    @Inject(MAT_DIALOG_DATA)
+    public data: {
+      Name: string,
+      Bio: string,
+      Birth: string,
+    }
+  ) {}
+}
